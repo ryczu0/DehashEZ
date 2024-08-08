367973ecbc3ccb12c3b174e05d99ce6eb0b145a1
jaul MK <ryzzu68@gmail.com>
2024-08-08 14:03:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 #======================================================================================================
-
-
 #              Don't touch anything unless you know exactly what you're doing.
-
-
 #======================================================================================================
 
 import hashlib, time, os, shutil
 import pyperclip as clipboard
 from colorama import Fore
 
-words = set()
+passwords = []
 
 #======================================================================================================
 
@@ -37,45 +33,22 @@ def printcenter(text):
 
 #======================================================================================================
 
-def bruteforce(hash, salt):
-    if len(hash) == 64:
-        for password in words:
-            password_hash = hashlib.sha256(password.encode()).hexdigest()
-            if password_hash == hash:
+def bruteforce(hash_str: str, salt: str = None):
+    # SHA256
+    if len(hash_str) == 64:
+        for password in passwords:
+            if hashlib.sha256(password.encode()).hexdigest() == hash_str:
                 return password
-    elif len(hash) == 86 or len(hash) == 85:
-        parts = hash.split("$")
-        salt1 = parts[2]
-        hash1 = parts[3]
-        for word in words:
-            var2 = hashlib.sha256(word.encode()).hexdigest()
-            final = hashlib.sha256((var2 + salt1).encode()).hexdigest()
-            if final == hash1:
-                return word
-    elif len(hash) == 128:
-        for word in words:
-            var2 = hashlib.sha512(word.encode()).hexdigest()
-            final = hashlib.sha512((var2 + salt).encode()).hexdigest()
-            if final == hash:
-                return word
-    elif "SHA256" in hash:
-        parts = hash.split("$")
-        salt = parts[1]
-        wow = parts[2]
-        for word in words:
-            word_hash = hashlib.sha256(hashlib.sha256(word.encode()).hexdigest().encode() + salt.encode()).hexdigest()
-            if word_hash == wow:
-                return word
-    elif "SHA512" in hash:
-        parts = hash.split("$")
-        salt = parts[1]
-        wow = parts[2]
-        for word in words:
-            passenc = hashlib.sha512(word.encode()).hexdigest()
-            word_hash = hashlib.sha512((passenc + salt).encode()).hexdigest()
-            if word_hash == wow:
-                return word
-    return hash
+            if salt is not None and (hashlib.sha256(password.encode() + salt.encode()).hexdigest() == hash_str or hashlib.sha256(hashlib.sha256(password.encode()).hexdigest().encode() + salt.encode()).hexdigest() == hash_str):
+                return password
+    # SHA512
+    elif len(hash_str) == 128:
+        for password in passwords:
+            if hashlib.sha512(password.encode()).hexdigest() == hash_str:
+                return password
+            if salt is not None and (hashlib.sha512(password.encode() + salt.encode()).hexdigest() == hash_str or hashlib.sha512(hashlib.sha512(password.encode()).hexdigest().encode() + salt.encode()).hexdigest() == hash_str):
+                return password
+    return hash_str
 
 #======================================================================================================
 
@@ -100,36 +73,34 @@ def main():
         printcenter(f"{Fore.RESET}Valid file.\n\n{Fore.BLUE}   Processing wordlist...")
         print()
         with open(wordlist, 'r', encoding="latin-1") as f:
-            lines = f.read().splitlines()
-            words.update(lines)
-            print(f"{Fore.LIGHTBLUE_EX}                             [LOG]{Fore.RESET} {Fore.LIGHTMAGENTA_EX}{len(words)}{Fore.RESET} passwords have been uploaded from {Fore.LIGHTMAGENTA_EX}",wordlist,"", end="\r")
+            passwords.extend([password.strip() for password in f])
+            print(f"{Fore.LIGHTBLUE_EX}                             [LOG]{Fore.RESET} {Fore.LIGHTMAGENTA_EX}{len(passwords)}{Fore.RESET} passwords have been uploaded from {Fore.LIGHTMAGENTA_EX}", wordlist, "", end="\r")
             print()
             print()
             while True:
-                hash, salt = "", ""
-                hash = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Enter hash: {Fore.RESET}")
+                hash_str, salt = "", ""
+                hash_str = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Enter hash: {Fore.RESET}")
                 print()
                 salt = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Enter salt{Fore.LIGHTBLACK_EX} (optional): {Fore.RESET}")
                 print()
                 printcenter(f"{Fore.LIGHTMAGENTA_EX}Decrypting...")
                 startTime = time.time()
-                final = bruteforce(hash, salt)
-                if final == hash:
-                    endTime = time.time()
-                    totalTime = (endTime - startTime)
-                    totalTime = round(totalTime, 3)
+                final = bruteforce(hash_str, salt)
+                endTime = time.time()
+                totalTime = round((endTime - startTime), 3)
+
+                if final == hash_str:
                     os.system("cls || clear")
                     printcenter(f"{Fore.LIGHTBLUE_EX}{logo}")
                     print()
                     printcenter(f"{Fore.LIGHTBLUE_EX}[LOG]{Fore.RESET} It was not possible to decrypt the hash.")
                     print()
-                    print(f"{Fore.LIGHTBLACK_EX}                                        Response time:",totalTime,"seconds.")
-                    ####
+                    print(f"{Fore.LIGHTBLACK_EX}                                        Response time:", totalTime, "seconds.")
                     print()
                     print(f"{Fore.LIGHTBLUE_EX}(1) {Fore.RESET}Go back")
                     print(f"{Fore.LIGHTBLUE_EX}(2) {Fore.RESET}Leave")
                     print()
-                    option = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Select ant option: {Fore.RESET}")
+                    option = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Select an option: {Fore.RESET}")
                     if option == "1":
                         main()
                     elif option == "2":
@@ -143,11 +114,7 @@ def main():
                         printcenter(f"{Fore.RESET}You must select a valid option!")
                         time.sleep(3)
                         main()
-                    ####
-                if final != hash:
-                    endTime = time.time()
-                    totalTime = (endTime - startTime)
-                    totalTime = round(totalTime, 3)
+                else:
                     os.system("cls || clear")
                     printcenter(f"{Fore.LIGHTBLUE_EX}{logo}")
                     print()
@@ -156,13 +123,13 @@ def main():
                     print()
                     printcenter(f"{Fore.LIGHTBLACK_EX}Result copied to clipboard.")
                     print()
-                    print(f"{Fore.LIGHTBLACK_EX}                                              Dehash in",totalTime,"seconds")
+                    print(f"{Fore.LIGHTBLACK_EX}                                              Dehash in", totalTime, "seconds")
                     print()
                     print()
                     print(f"{Fore.LIGHTBLUE_EX}(1) {Fore.RESET}Go back")
                     print(f"{Fore.LIGHTBLUE_EX}(2) {Fore.RESET}Leave")
                     print()
-                    option = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Select and option: {Fore.RESET}")
+                    option = input(f"{Fore.LIGHTMAGENTA_EX} [»] {Fore.LIGHTBLUE_EX}Select an option: {Fore.RESET}")
                     if option == "1":
                         main()
                     elif option == "2":
@@ -176,7 +143,6 @@ def main():
                         printcenter(f"{Fore.RESET}You must select a valid option!")
                         time.sleep(3)
                         main()
-
 
 #======================================================================================================
 
